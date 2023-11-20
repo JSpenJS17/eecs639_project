849ab296205081bf589c75b11555e3d5a18b18d8
JSpenJS17 <jspencer.js17@gmail.com>
2023-11-20 13:31:53 -0600

    Initial Commit     - Has FEM, BEM, and trap methods.         - Trap needs some debugging, doesn't give accurate results,           they're always too low.

---
+++ BEM.m
@@ -0,0 +1,12 @@
+function [ys, ts] = BEM(t_0, y_0, h, t_max, func, maxiter, tol)
+    ts = t_0:h:t_max;
+    ys = [y_0; zeros(t_max/h, 1)];
+    
+    for k = 1:length(ts)-1
+        i = 0;
+        while abs(ys(k) + h*func(ts(k+1), ys(k+1) - ys(k+1))) > tol && i < maxiter
+            ys(k+1) = ys(k) + h*func(ts(k+1), ys(k+1));
+            i = i + 1;
+        end
+    end
+end
---
+++ f.m
@@ -0,0 +1,6 @@
+function [y_prime] = f(t, y)
+%F Summary of this function goes here
+%   Detailed explanation goes here
+y_prime = y;
+end
+
