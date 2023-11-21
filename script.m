2fa266cb2a911b44780bfd554e60695c4b4fb24f
Harlan Williams <hrw@ku.edu>
2023-11-20 19:24:02 -0600

    eulers_method that evaluates higher ODEs

--- script.m
+++ script.m
@@ -40,3 +40,20 @@ syms x
 g = exp(x);
 figure;
 fplot(g, [t_0 t_max])
+
+
+% TESTING forward_euler_multi
+h = 0.025;
+t_0 = 0;
+t_max = 3.5;
+y_0 = [0; 1];
+
+[t, y] = forward_euler_multi(h, t_0, t_max, y_0, @u);
+figure;
+fplot(t, y(2, :));
+
+
+% y'' = -y
+function [z] = u(t, y)
+    z = -y(1);
+end
