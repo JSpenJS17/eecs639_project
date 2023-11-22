f8301909ebdd8a723bf0933a8754d952cdbf27ce
JSpenJS17 <jspencer.js17@gmail.com>
2023-11-21 18:08:32 -0600

    N-Order FEM and BEM     - Added comments to f.m, BEM.m, and FEM.m

--- FEM.m
+++ FEM.m
@@ -1,7 +1,8 @@
 function [ys, ts] = FEM(t_0, y_0, h, t_max, f)
+    % detailed preamble comment goes here
     ts = (t_0:h:t_max)';
     ys = [y_0;zeros(length(ts)-1, length(y_0))];
-    
+
     for k = 1:length(ts)-1
         ys(k+1, :) = ys(k, :) + h*f(ts(k, :), ys(k, :));
 
--- f.m
+++ f.m
@@ -1,6 +1,7 @@
 function [z] = f(t, y)
-%F Summary of this function goes here
-%   Detailed explanation goes here
-z = [y(2) -y(1)];
+    % z = [u2, f(t, y)], like from class.
+    % in this case, f(t, y) is just -y, meaning that the function is either
+    % cos(x) or sin(x), depending on the initial conditions.
+    z = [y(2) -y(1)];
 end
 
