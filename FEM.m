a02322423dbdd856ff8f9fd40ae7fa8d7fa591d5
Harlan Williams <hrw@ku.edu>
2023-11-30 20:11:49 -0600

    implement RK4 and predictor/corrector, standardize FEM, write some tests

--- FEM.m
+++ FEM.m
@@ -1,10 +1,11 @@
+% Forward Eulers method
 function [ys, ts] = FEM(t_0, y_0, h, t_max, f)
-    % detailed preamble comment goes here
-    ts = (t_0:h:t_max)';
-    ys = [y_0;zeros(length(ts)-1, length(y_0))];
+    n = length(y_0);
+    ts = t_0:h:t_max;
+    ys = zeros(n, length(ts));
+    ys(:, 1) = y_0;
 
     for k = 1:length(ts)-1
-        ys(k+1, :) = ys(k, :) + h*f(ts(k, :), ys(k, :));
-
+        ys(:, k+1) = ys(:, k) + h*f(ts(k), ys(:, k));
     end
 end
---
+++ RK4.m
@@ -0,0 +1,15 @@
+% Runge-Kutta 4th order method
+function [ys, ts] = RK4(t_0, y_0, h, t_max, f)
+    n = length(y_0);
+    ts = t_0:h:t_max;
+    ys = zeros(n, length(ts));
+    ys(:, 1) = y_0;
+
+    for k = 1:length(ts)-1
+        k1 = f(ts(k), ys(:, k));
+        k2 = f(ts(k) + h/2, ys(:, k) + k1*(h/2));
+        k3 = f(ts(k) + h/2, ys(:, k) + k2*(h/2));
+        k4 = f(ts(k) + h, ys(:, k) + k3*h);
+        ys(:, k+1) = ys(:, k) + (h/6) * (k1 + 2*k2 + 2*k3 + k4);
+    end
+end
