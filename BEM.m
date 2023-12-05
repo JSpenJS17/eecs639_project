2ce27f946c9ffb5f35a16ed977277a5d2d61bc36
Harlan Williams <harlanrhwilliams@gmail.com>
2023-12-05 15:26:27 -0600

    corrected root-finding methods in BEM; not working still

--- BEM.m
+++ BEM.m
@@ -7,21 +7,34 @@ function [ys, ts] = BEM(t_0, y_0, h, t_max, f, J)
 
     for k = 1:n
         % use the previos value of y as the guess for the next one
-        ys(:, k+1) = newtons_method(ts(k), ys(:, k), ys(:, k), tol, f, J);
+        ys(:, k+1) = ys(:, k);
+        ys(:, k+1) = newtons_method(ts(k+1), ys(:, k+1), h, tol, f, J);
+        % ys(:, k+1) = fixed_point(ts(k+1), ys(:, k+1), h, tol, f);
     end
 end
 
-function [y_next] = newtons_method(t, y_k, y_guess, tol, f, J)
+function [y_next] = newtons_method(t, y0, h, tol, f, J)
     max_iters = 100;
     iters = 0;
-    y_next = y_guess;
+    y_next = y0;
 
-    while norm(f(t, y_next), inf) > tol && iters < max_iters
-        J_tyk = J(t, y_next);
-        % replace this with LUP decomp when i actually get it working
-        s_k = J_tyk \ -f(t, y_next);
+    while norm(y_next - (y0 + h*f(t, y_next)), inf) > tol && iters < max_iters
+        [L, U, P] = lu(J(t, y_next));
+        v = L\P'*-(y_next - y0 - h*f(t, y_next));
+        s_k = U\v;
 
-        y_next = y_k + s_k;
+        y_next = y_next + s_k;
+        iters = iters + 1;
+    end
+end
+
+function [y_next] = fixed_point(t, y0, h, tol, f)
+    max_iters = 100;
+    iters = 0;
+    y_next = y0;
+
+    while norm(y_next - (y0 + h*f(t, y_next)), inf) > tol && iters < max_iters
+        y_next = y0 + h*f(t, y_next);
         iters = iters + 1;
     end
 end
