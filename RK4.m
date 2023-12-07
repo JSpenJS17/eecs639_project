aac0940dcadfbb7ada31ee76b52f094bc31e9ebc
Harlan Williams <harlanrhwilliams@gmail.com>
2023-12-06 23:18:06 -0600

    cast constants to doubles

--- RK4.m
+++ RK4.m
@@ -7,9 +7,9 @@ function [ys, ts] = RK4(t_0, y_0, h, t_max, f)
 
     for k = 1:length(ts)-1
         k1 = f(ts(k), ys(:, k));
-        k2 = f(ts(k) + h/2, ys(:, k) + k1*(h/2));
-        k3 = f(ts(k) + h/2, ys(:, k) + k2*(h/2));
+        k2 = f(ts(k) + h/2., ys(:, k) + k1*(h/2.));
+        k3 = f(ts(k) + h/2., ys(:, k) + k2*(h/2.));
         k4 = f(ts(k) + h, ys(:, k) + k3*h);
-        ys(:, k+1) = ys(:, k) + (h/6) * (k1 + 2*k2 + 2*k3 + k4);
+        ys(:, k+1) = ys(:, k) + (h/6.) * (k1 + 2.*k2 + 2.*k3 + k4);
     end
 end
